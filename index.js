6f32c4464f56ce455d06ad460ad137e35fccbbf4
Chandler Craig <hellochandlercraig@gmail.com>
2022-01-11 11:19:01 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, str) {
+  const new_arr = []
+  for (let i = 0; i < arr.length; i++) {
+    const messg = `Thank you, ${arr[i]}, for the wonderful ${str} gift!`;
+    new_arr.push(messg)
+  }
+  return new_arr
+}
+
+function countDown(num) {
+  let i = 0;
+  while (i <= num) {
+    console.log(num);
+    num--;
+  }
+}
